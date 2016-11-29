b955ddf6670b972cf4bd2df75d1b860b8c41dadd
Tranz Zhang <quan.zhang@mail.scut.edu.cn>
2016-11-29 21:26:01 +0800

    Basic project setting

---
+++ MyPassword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPassword
+//
+//  Created by chance on 29/11/2016.
+//  Copyright © 2016 bychance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPassword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPassword
+//
+//  Created by chance on 29/11/2016.
+//  Copyright © 2016 bychance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
