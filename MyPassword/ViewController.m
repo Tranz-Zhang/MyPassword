f0d781d0c208a7fd14763bf7e8c4eba08bba1caa
tranz <tranz.zhang@gmail.com>
2016-11-29 21:41:01 +0800

    update readme

--- MyPassword/ViewController.m
+++ MyPassword/ViewController.m
@@ -16,13 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    // 1. Test aes256 encrpytion and decription
+    // 2. Build up database
+    // 3. Design interface
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MyPassword
+
 A simple app use AES256 encryption to manage your password
+
+The AES256 encryption is implemented by [RNCryptor](https://github.com/RNCryptor/RNCryptor).
+
+---
